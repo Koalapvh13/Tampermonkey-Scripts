77f7ecc5442f4e1b051c91bc8acfd2230becf2a1
Matheus Dias Vieira <matheuspvh13@gmail.com>
2020-10-14 21:02:23 -0400

    Add UserScript info to devto-autolike.js

---
+++ Web Scripts/devto-autolike.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         DevTo AutoLike
+//@homepageURL https://github.com/Koalapvh13/Tampermonkey-Scripts
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Script to automatically like Dev.to Posts Opened in Browser 
+// @license MIT
+//@icon https://lh3.googleusercontent.com/zw07Qyfb7MnF8J9pZJ6eYheztKq1shP1j6tUqyDYZj6R60nNrrPrFZvC9k5JIe2m9t2GfQLbXg=w128-h128-e365-rj-sc0x00ffffff
+// @author       Matheus Dias Vieira
+//@copyright 2020, Matheus Dias Vieira (https://github.com/Koalapvh13)
+// @match        http*://*.dev.to/*
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
